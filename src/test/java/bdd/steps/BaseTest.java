f13cf7746e483252d92d18875a0c845b719e6a12
yinghao_niu <yinghao_niu@126.com>
2016-06-26 01:52:17 +0800

    user register by bdd driven

---
+++ src/test/java/bdd/steps/BaseTest.java
@@ -0,0 +1,93 @@
+package bdd.steps;
+
+import junit.framework.TestCase;
+import org.aspectj.lang.annotation.After;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest extends TestCase {
+    protected WebDriver driver;
+    protected String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    public BaseTest() {
+        System.setProperty(
+                "webdriver.chrome.driver",
+                "D:\\_dev_tools\\chromedriver_win32\\chromedriver.exe");
+//        driver = new FirefoxDriver();
+
+
+//        ChromeOptions options = new ChromeOptions();
+//        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+//        capabilities.setCapability("chrome.binary", "C:\\Users\\yinghao_niu\\AppData\\Local\\360Chrome\\Chrome\\Application\\360chrome.exe");
+//        options.addArguments("window-size=1024,768");
+//
+//        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+//        driver = new ChromeDriver(capabilities);
+        driver = new FirefoxDriver();
+
+        baseUrl = "http://127.0.0.1:1234";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+/*
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://test.qa.dhgate.com:8888/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+*/
+
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            }
+            else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    public boolean equals(Object obj1, Object obj2) {
+        return obj1.equals(obj2);
+    }
+}
