27c330a2d3b91eac26e60c6f2926e03774ab167c
yinghao_niu <yinghao_niu@126.com>
2016-06-25 13:36:54 +0800

    user register

--- src/test/java/bdd/Runner.java
+++ src/test/java/bdd/Runner.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.ContextConfiguration;
 @CucumberOptions(
         features = {"src\\test\\java\\bdd\\feature"},
         glue = {"com.cdi.igs.adapter.cucumber", "cucumber.api.spring", "bdd.steps"},
-        tags = {"@smoke"},
+        tags = {"@test"},
         format = {"json:target/integration_cucumber.json"}
 )
 public class Runner {
