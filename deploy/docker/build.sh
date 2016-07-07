26be2d04fe8af0a89818b97148d6286c134557cd
coding1628 <coding1628@163.com>
2016-07-08 00:07:22 +0800

    some style

--- deploy/docker/build.sh
+++ deploy/docker/build.sh
@@ -3,4 +3,5 @@ docker build -t jdk -f Dockerfile-jdk .
 docker build -t tomcat -f Dockerfile-tomcat .
 docker build -t postgres -f Dockerfile-postgres .
 docker build -t data -f Dockerfile-data .
-docker build --no-cache -t star -f Dockerfile-star .
+docker build  -t star -f Dockerfile-star .
+#docker build --no-cache -t star -f Dockerfile-star .
--- deploy/docker/run.sh
+++ deploy/docker/run.sh
@@ -1,4 +1,9 @@
+# db
 docker run -d -v /var/lib/postgresql/9.3/main --name c_data --entrypoint bash postgres
 docker run  -d -h dbServer --name dbPostgres -p 5432:5432 --volumes-from c_data postgres
-docker run -d --name 'star' --link dbPostgres -p 80:8080 star 
+
+# star
+#JO="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -javaagent:/opt/tomcat/bin/jrebel.jar -Xbootclasspath/p:/tmp/hostTmp/jrebelTmp.jar -Djava.rmi.server.hostname=127.0.0.1 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false" 
+JO="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005  -Djava.rmi.server.hostname=127.0.0.1 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false" 
+docker run  --name 'star' --link dbPostgres -p 80:8080 -e JAVA_OPTS="$JO" -v /tmp:/tmp/hostTmp -p 2200:22 star 
 docker logs --tail=20 -f star
