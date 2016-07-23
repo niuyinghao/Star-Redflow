cb47e63b589499b5cb804ff408a46b7d56996d5c
coding1628 <coding1628@coding.com>
2016-07-23 08:35:47 -0400

    docker

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,15 +1,13 @@
-cd ..
-git pull
 mvn -DskipTests=true package
 mv /root/p1628/target/project-1.0-SNAPSHOT.war  deploy/docker/
 cd deploy/docker && {
-	docker build --no-cache -t star -f Dockerfile-star . && rm project-1.0-SNAPSHOT.war 
+	docker build --no-cache -t star -f Dockerfile-star . && rm project-1.0-SNAPSHOT.war  &&{
+		#run 
+  	(docker rm -f star  || echo ) && docker run -d --name 'star' --link dbPostgres -p 80:8080 -e JAVA_OPTS="$JO" -v /tmp:/tmp/hostTmp -p 2200:22 star
+echo		docker logs --tail=20 -f star
+	}
 }
 
 
 
-#run 
-docker rm -f star
-docker run  --name 'star' --link dbPostgres -p 80:8080 -e JAVA_OPTS="$JO" -v /tmp:/tmp/hostTmp -p 2200:22 star
-docker logs --tail=20 -f star
 
