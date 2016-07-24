04a19605b781ab7191ff441ab94a001ab7fbbe16
coding1628 <coding1628@163.com>
2016-07-24 15:23:05 +0800

    pre set data deploy

--- deploy/docker/content/content.sql
+++ deploy/docker/content/content.sql
@@ -1,30 +1,30 @@
-INSERT INTO public.wave (id, create_time, buried, content, flag, sign, creator, mound_time, mound) VALUES (-1, NULL , false, '<span class="Apple-tab-span" style="white-space: pre;">	<span style="font-size: large;">	</span></span><span
-        style="font-size: large;">风平浪静，架着小船行驶在无际的海上，欲寻找停泊的地方休憩。&nbsp;</span>
-<div><span style="font-size: large;"><span class="Apple-tab-span" style="white-space:pre">	</span>天色渐渐阴沉，原来有了乌云，似有暴雨来临。伴着越来越强的狂风，海面荡起起伏的浪。</span>
+INSERT INTO public.wave (id, create_time, buried, content, flag, sign, creator, mound_time, mound) VALUES (-1, NULL , false, '<span class=\"Apple-tab-span\" style=\"white-space: pre;\">	<span style=\"font-size: large;\">	</span></span><span
+        style=\"font-size: large;\">风平浪静，架着小船行驶在无际的海上，欲寻找停泊的地方休憩。&nbsp;</span>
+<div><span style=\"font-size: large;\"><span class=\"Apple-tab-span\" style=\"white-space:pre\">	</span>天色渐渐阴沉，原来有了乌云，似有暴雨来临。伴着越来越强的狂风，海面荡起起伏的浪。</span>
 </div>
-<div><span style="font-size: large;"><span class="Apple-tab-span" style="white-space: pre;">	</span>肥鲸在水中上下跃入跃出，搅动海面。</span>
+<div><span style=\"font-size: large;\"><span class=\"Apple-tab-span\" style=\"white-space: pre;\">	</span>肥鲸在水中上下跃入跃出，搅动海面。</span>
 </div>
-<div><span style="font-size: large;">&nbsp; &nbsp; &nbsp;</span><img
-        src="/resources/STAR-0000/sea.jpg"></div>', 1, null, 1, null, null);
+<div><span style=\"font-size: large;\">&nbsp; &nbsp; &nbsp;</span><img
+        src=\"/resources/STAR-0000/sea.jpg\"></div>', 1, null, 1, null, null);
 
 
 
 
-INSERT INTO public.flower (id, create_time, buried, content, flag, sign, creator, mound_time, mound) VALUES (-2, NULL , false, '<span style="font-size: large;"><span class="Apple-tab-span"
-                                      style="white-space:pre">	</span>欲往青山的路上，遇见黛鹿，为其美容吸引。</span>
-<div><span style="font-size: large;"><span class="Apple-tab-span" style="white-space:pre">	</span>追随其到一花林，忽觉身体正在陷入，原来花下是沼泽之地。</span>
+INSERT INTO public.flower (id, create_time, buried, content, flag, sign, creator, mound_time, mound) VALUES (-2, NULL , false, '<span style=\"font-size: large;\"><span class=\"Apple-tab-span\"
+                                      style=\"white-space:pre\">	</span>欲往青山的路上，遇见黛鹿，为其美容吸引。</span>
+<div><span style=\"font-size: large;\"><span class=\"Apple-tab-span\" style=\"white-space:pre\">	</span>追随其到一花林，忽觉身体正在陷入，原来花下是沼泽之地。</span>
 </div>
-<div style="font-size: 10pt; text-align: center;"><img src="/resources/STAR-0000/swamp.jpg"></div>', 1, null, 1, null, null);
+<div style=\"font-size: 10pt; text-align: center;\"><img src=\"/resources/STAR-0000/swamp.jpg\"></div>', 1, null, 1, null, null);
 
-INSERT INTO public.stone (id, create_time, buried, content, flag, sign, creator) VALUES (-3, NULL , false, '<span style="font-size: large;"><span class="Apple-tab-span" style="white-space:pre">	</span>青山脚下抬头望去，山岗上丛林繁密，道路错综弯延，峰顶上山鹰腾翔。</span>
-<div><span style="font-size: large;"><span class="Apple-tab-span" style="white-space:pre">	</span>欲攀登到山顶，一览风景，徒步从这里开始。</span>
+INSERT INTO public.stone (id, create_time, buried, content, flag, sign, creator) VALUES (-3, NULL , false, '<span style=\"font-size: large;\"><span class=\"Apple-tab-span\" style=\"white-space:pre\">	</span>青山脚下抬头望去，山岗上丛林繁密，道路错综弯延，峰顶上山鹰腾翔。</span>
+<div><span style=\"font-size: large;\"><span class=\"Apple-tab-span\" style=\"white-space:pre\">	</span>欲攀登到山顶，一览风景，徒步从这里开始。</span>
 </div>
-<div style="font-size: 10pt; text-align: center;"><img src="/resources/STAR-000/hillock.jpg"></div>', 1, null, 1);
+<div style=\"font-size: 10pt; text-align: center;\"><img src=\"/resources/STAR-000/hillock.jpg\"></div>', 1, null, 1);
 
-INSERT INTO public.wish (id, create_time, buried, content, flag, sign, creator) VALUES (-4, NULL , false, '<span style="font-size: large;"><span class="Apple-tab-span" style="white-space:pre">	</span>仰望星空，星星闪烁，静谧。</span>
-<div><span style="font-size: large;"><span class="Apple-tab-span" style="white-space:pre">	</span>这时忘记了其他事情，感受宁静与平和。</span>
+INSERT INTO public.wish (id, create_time, buried, content, flag, sign, creator) VALUES (-4, NULL , false, '<span style=\"font-size: large;\"><span class=\"Apple-tab-span\" style=\"white-space:pre\">	</span>仰望星空，星星闪烁，静谧。</span>
+<div><span style=\"font-size: large;\"><span class=\"Apple-tab-span\" style=\"white-space:pre\">	</span>这时忘记了其他事情，感受宁静与平和。</span>
 </div>
-<div style="text-align: center;"><img src="/resources/STAR-0000/star.jpeg"></div>', 1, null, 1);
+<div style=\"text-align: center;\"><img src=\"/resources/STAR-0000/star.jpeg\"></div>', 1, null, 1);
 
 INSERT INTO public.heart_symbol (id, age, belong) VALUES (-5, 11, -1);
 INSERT INTO public.heart_symbol (id, age, belong) VALUES (-6, 18, -2);
--- deploy/docker/run.sh
+++ deploy/docker/run.sh
@@ -1,9 +1,11 @@
 # db
 docker run -d -v /var/lib/postgresql/9.3/main --name c_data --entrypoint bash postgres
+# pics
+docker run -d -v /opt/tomcat/webapps/ROOT/resources --name c_pic_data --entrypoint bash star
 docker run  -d -h dbServer --name dbPostgres -p 5432:5432 --volumes-from c_data postgres
 
 # star
 #JO="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -javaagent:/opt/tomcat/bin/jrebel.jar -Xbootclasspath/p:/tmp/hostTmp/jrebelTmp.jar -Djava.rmi.server.hostname=127.0.0.1 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false" 
 JO="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005  -Djava.rmi.server.hostname=127.0.0.1 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false" 
-docker run  --name 'star' --link dbPostgres -p 80:8080 -e JAVA_OPTS="$JO" -v /tmp:/tmp/hostTmp -p 2200:22 star 
+docker run  --name 'star' --link dbPostgres --volumes-from c_pic_data -p 80:8080 -e JAVA_OPTS="$JO" -v /tmp:/tmp/hostTmp -p 2200:22 star
 docker logs --tail=20 -f star
